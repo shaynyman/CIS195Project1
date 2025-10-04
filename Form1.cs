6d30e9c100f04e7a7114a940572d0b4f85c408d4
shaynyman <shay.nyman@southeasttech.edu>
2025-10-04 15:03:21 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Shayna Nyman
+//Project1
+//09/28/25
+
+
+namespace Project1
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void SixMonthAmountButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Variable to hold Value of Investment.
+                double ValueOfInvestment;
+
+                //Calculate Value of Investment.
+                ValueOfInvestment = double.Parse(originalInvestmentAmountTextBox.Text) +
+                (double.Parse(annualRateOfReturnTextBox.Text) / 2);
+
+                //Display Value of Investment.
+                amountOfInvestOutputLabel.Text = ValueOfInvestment.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                //Display Error Message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+
+        private void OneYearAmountButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Variable to hold value of investment at 1 year.
+                double ValueOfInvestment1;
+
+                //Calculate Value of Investment.
+                ValueOfInvestment1 = double.Parse(originalInvestmentAmountTextBox.Text) +
+                double.Parse(annualRateOfReturnTextBox.Text);
+
+                //Display Value of Investment.
+                amountOfInvestOutputLabel.Text = ValueOfInvestment1.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                //Display Error Message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void TwoYearInvestmentButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Variable to hold Value of Investment at 2 years.
+                double ValueOfInvestment;
+
+                //Calculate Value of Investment.
+                ValueOfInvestment = double.Parse(originalInvestmentAmountTextBox.Text) +
+                (double.Parse(annualRateOfReturnTextBox.Text)) * 2;
+
+                //Display Value of Investment.
+                amountOfInvestOutputLabel.Text = ValueOfInvestment.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                    //Display Error Message
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        private void FiveYearInvestmentButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Variable to hold Value of Investment.
+                double ValueOfInvestment;
+
+                //Calculate Value of Investment.
+                ValueOfInvestment = double.Parse(originalInvestmentAmountTextBox.Text) +
+                (double.Parse(annualRateOfReturnTextBox.Text)) * 5;
+
+                //Display Value of Investment.
+                amountOfInvestOutputLabel.Text = ValueOfInvestment.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                //Display Error Message
+                MessageBox.Show(ex.Message);
+            }
+            }
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            //Clear the TextBoxes.
+            originalInvestmentAmountTextBox.Text = "";
+            annualRateOfReturnTextBox.Text = "";
+
+            //Clear the OutputLabel.
+            amountOfInvestOutputLabel.Text = "";
+
+            //Set the focus.
+            originalInvestmentAmountTextBox.Focus();
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            //Close the form.
+            this.Close();
+        }
+    }   
+}
---
+++ Form3.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Name_Formatter
+{
+    public partial class nameFormatter : Form
+    {
+        public nameFormatter()
+        {
+            InitializeComponent();
+        }
+
+        private void titleFirstMidLastButton_Click(object sender, EventArgs e)
+        {
+            //Declares a string variable to hold formatted name.
+            string titleFirstMidLast;
+
+            //Combine the names with a space in between. Assign 
+            //the result to the titleFirstMidLast variable.
+            titleFirstMidLast = titleTextBox.Text + " " + firstNameTextBox.Text + 
+            " " + middleNameTextBox.Text + " " + lastNameTextBox.Text;
+
+            //Show the titleFirstMidLast variable in the nameOutputLabel control.
+            nameOutputLabel.Text = titleFirstMidLast;
+        }
+
+        private void firstMidLastButton_Click(object sender, EventArgs e)
+        {
+            //Declares a string variable to hold the formatted name.
+            string firstMidLast;
+
+            //Combine the names with spacing in between. Assign
+            //the result to the firstMidLast variable.
+            firstMidLast = firstNameTextBox.Text + " " + middleNameTextBox.Text +
+            " " + lastNameTextBox.Text;
+
+            //Show the firstMidLast variable in the nameOutputLabel control.
+            nameOutputLabel.Text = firstMidLast;
+        }
+
+        private void firstLastButton_Click(object sender, EventArgs e)
+        {
+            //Declares a string variable to hold the formatted name.
+            string firstLast;
+
+            //Combine the names with spacing in between. Assign
+            //the result to the firstLast variable.
+            firstLast = firstNameTextBox.Text + " " + lastNameTextBox.Text;
+
+            //Show the firstLast variable in the nameOutputLabel control.
+            nameOutputLabel.Text = firstLast;
+        }
+
+        private void lastFirstMidButon_Click(object sender, EventArgs e)
+        {
+            //Declares a string variable to hold the formatted name.
+            string lastFirstMid;
+
+            //Combine the names with spacing in between. Assign the
+            //result to the lastFirstMid variable.
+            lastFirstMid = lastNameTextBox.Text + "," + firstNameTextBox.Text +
+            " " + middleNameTextBox.Text;
+
+            //Show the lastFirstMid variable in the nameOutputLabel control.
+            nameOutputLabel.Text = lastFirstMid;
+        }
+
+        private void lastFirstButton_Click(object sender, EventArgs e)
+        {
+            //Declares a variable to hold the formatted name.
+            string lastFirst;
+
+            //Combine the names with spacing in between. Assign the
+            //result to the lastFirst variable.
+            lastFirst = lastNameTextBox.Text + "," + firstNameTextBox.Text;
+
+            //Show the lastFirst variable in the nameOutputLabel control.
+            nameOutputLabel.Text = lastFirst;
+
+        }
+
+        private void lastFirstMidTitleButton_Click(object sender, EventArgs e)
+        {
+            //Declares a variable to hold the formatted name.
+            string lastFirstMidTitle;
+
+            //Combine the names with spacing in between. Assign the 
+            //result to the lastFirstMidTitle variable.
+            lastFirstMidTitle = lastNameTextBox.Text + "," + firstNameTextBox.Text +
+            " " + middleNameTextBox.Text + "," + titleTextBox.Text;
+
+            //Show the lastFirstMidTitle variable in the nameOutputLabel control.
+            nameOutputLabel.Text = lastFirstMidTitle;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Close the form.
+            this.Close();
+        }
+    }
+}
